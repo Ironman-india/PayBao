7a5515c6534b92250399282f3d1a9d9e38d658b0
senchan sue <senchansue@gmail.com>
2016-06-21 17:07:05 +0200

    Initial Commit

---
+++ 记账宝/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  记账宝
+//
+//  Created by Senchan on 16/6/21.
+//  Copyright © 2016年 Senchan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 记账宝/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  记账宝
+//
+//  Created by Senchan on 16/6/21.
+//  Copyright © 2016年 Senchan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
