9532b18a3ed890a120d33d6fee0f471da291dc3c
senchan sue <senchansue@gmail.com>
2016-06-22 12:41:55 +0200

    Create the data class and CSV reader

---
+++ 记账宝/CSVReader.swift
@@ -0,0 +1,54 @@
+//
+//  CSVReader.swift
+//  PayBao
+//
+//  Created by Senchan on 16/6/22.
+//  Copyright © 2016年 Senchan. All rights reserved.
+//
+
+import Foundation
+import SwiftCSV
+
+class Data:NSObject {
+    var date:timeval?
+    var event:String?
+    var who:String?
+    var cost:String?
+}
+
+/**
+ CSVReader
+ - parameter fileName: data.csv
+ - parameter completionHandle: completionHandle
+ */
+
+class CSVReader {
+    /**
+     CSV读取
+     
+     - parameter fileName:         文件名
+     - parameter completionHandle: completionHandle
+     */
+    class func loadDataFromCSV<T:NSObject>(fileName:String,completionHandle:([T])->Void){
+        //异步读取
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: "csv")
+            let csvURL = NSURL(fileURLWithPath: filePath!)
+            do{
+                let csv = try CSV(url: csvURL)
+                let dataArray = csv.rows.map({ (dic:[String:String]) -> T in
+                    let data = T()
+                    dic.forEach({ (key,value) in
+                        data.setValue(value, forKey: key)
+                    })
+                    return data
+                })
+                dispatch_async(dispatch_get_main_queue(), {
+                    completionHandle(dataArray)
+                })
+            }catch{
+                completionHandle([])
+            }
+        })
+    }
+}
